fc33844f66dab6dd73b6b4752fe43e4afa82b90e
Le Quang Chinh <chinh@Les-MacBook-Pro.local>
2016-12-06 14:30:54 +0700

    I just finished my freaking pokedex app woohoo

---
+++ pokedex/PokemonDetailVN.swift
@@ -0,0 +1,35 @@
+//
+//  PokemonDetailVN.swift
+//  pokedex
+//
+//  Created by Le Quang Chinh on 11/8/16.
+//  Copyright © 2016 Chinh Le. All rights reserved.
+//
+
+import UIKit
+
+class PokemonDetailVN: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
